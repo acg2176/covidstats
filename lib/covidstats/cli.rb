7a896f95b52ddee5d6c466b00eac17ae14531665
Anna Guerrero <acg2176@columbia.edu>
2020-05-23 22:45:30 +0000

    modify cli.rb

--- lib/covidstats/cli.rb
+++ lib/covidstats/cli.rb
@@ -1,5 +1,6 @@
 class Covidstats::CLI 
   def call
-    puts "Welcome to the daily corona tracker!"
+    puts "Welcome to the daily corona tracker! This CLI app allows you to check various data regarding the ongoing coronavirus and includes information from numerous countries."
+    #A few things you can do on this app
   end
 end
