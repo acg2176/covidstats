91c1818fa693bc2f2841e031b6b141450c6528e4
Anna Guerrero <acg2176@columbia.edu>
2020-05-23 22:13:58 +0000

    added require for files

--- lib/covidstats.rb
+++ lib/covidstats.rb
@@ -1,4 +1,11 @@
-require "covidstats/version"
+require 'pry'
+require 'httparty'
+
+
+require_relative "covidstats/version"
+require_relative "covidstats/cli"
+require_relative "covidstats/api"
+require_relative "covidstats/covid"
 
 module Covidstats
   class Error < StandardError; end
---
+++ lib/covidstats/cli.rb
@@ -0,0 +1,5 @@
+class Covidstats::CLI 
+  def call
+    puts "Welcome to the daily corona tracker!"
+  end
+end
