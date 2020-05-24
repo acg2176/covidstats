5bcce62e15d4dcdc9699859a9bc2d37a27ed174a
Anna Guerrero <acg2176@columbia.edu>
2020-05-24 22:33:37 +0000

    edit cli and covid class

--- lib/covidstats/cli.rb
+++ lib/covidstats/cli.rb
@@ -1,11 +1,14 @@
 class Covidstats::CLI 
   def call
-    puts "Welcome to the daily corona tracker! This CLI app provides real time data regarding the ongoing coronavirus and includes information from numerous countries. As the USA has become the hardest hit country with nearly 100,000 deaths as of May 2020, this gem includes additional data on USA cases by states."
+    puts "Welcome to the daily corona tracker! This CLI app provides real time data regarding the ongoing coronavirus pandemic and includes information from numerous countries. As the USA has become the hardest hit country with nearly 100,000 deaths as of May 2020, this gem includes additional data on USA cases by states."
     #A few things you can do on this app
     world_stats
   end
   
   def world_stats
-    puts "Would you like to see the world statistics for today? (y/n)"
+    puts "To begin, would you like to see the world statistics for today? (y/n)"
+    input = gets.strip
+    #call a function from the covidstats class
   end
+  
 end
